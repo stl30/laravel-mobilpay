60a4aa7569cb1e4c2561614881f913c4c0b16e3e
S_Leo <stoea.leontin@gmail.com>
2021-01-27 13:27:37 +0200

    update package to use https://github.com/mobilpay/composer

--- src/LaravelMobilpay/LaravelMobilpayCustomActionsAndNotifications.php
+++ src/LaravelMobilpay/LaravelMobilpayCustomActionsAndNotifications.php
@@ -5,15 +5,13 @@ namespace App\LaravelMobilpay;
 use Netopia\Payment\Request\Card;
 use Netopia\Payment\Request\PaymentAbstract;
 use Stl30\LaravelMobilpay\CustomActionsAndNotifications;
-use Stl30\LaravelMobilpay\Mobilpay\Payment\Request\Mobilpay_Payment_Request_Abstract;
-use Stl30\LaravelMobilpay\Mobilpay\Payment\Request\Mobilpay_Payment_Request_Card;
 use Stl30\LaravelMobilpay\MobilpayTransaction;
 
 class LaravelMobilpayCustomActionsAndNotifications extends CustomActionsAndNotifications
 {
     public function beforeCreatingTransaction(Card $mobilpayRequestObject, $customDataParameter = '')
     {
-        
+
     }
 
     public function afterCreatingTransaction(MobilpayTransaction $transaction, $addTransactionIsSuccessful)
